eda0679b4f7ac1eae073577bf442307819adb304
Peter Robinson <peter.robinson@jax.org>
2022-12-06 10:36:55 -0500

    skeleton implementation of column mapper

--- pyphetools/column_mapper.py
+++ pyphetools/column_mapper.py
@@ -10,5 +10,20 @@ class ColumnMapper:
             self._custom_map_d = custom_map_d
 
 
-    def map_cell(self, cell_contents) -> List:
-        return []
+    def map_cell(self, cell_contents, id_to_primary_d, label_to_id_d) -> List:
+        """
+        This method should not be used by client code. Instead, it is called
+        by the CohortEncoder object, which should provide the id_to_primary_d, label_to_id_d
+        objects representing the automatic HPO dictionaries. It is designed to take
+        the contents of a single cell in a Supplemental Table and to parse out 
+        HPO terms.
+        For now, we use a naive implementation that searches for exact matches. Moving forward
+        it should be possible to implement the algorithm of fenominal that removes stop words
+        and searches for opntology concepts in which the remaining tokens occur in any order.
+        """
+        positive_term_id_hits = []
+        for k, v in self._custom_map_d.items():
+            if k in cell_contents:
+                positive_term_id_hits.append(v)
+
+        return positive_term_id_hits
