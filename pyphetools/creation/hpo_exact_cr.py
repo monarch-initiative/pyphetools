2093c45fab7a8612c6497dde4e67025603f84777
Lex Dingemans <a.dingemans@radboudumc.nl>
2023-01-23 10:43:24 +0100

    fixed bug where ``custom_d`` was ignored and the default HPO dict was used

--- pyphetools/creation/hpo_exact_cr.py
+++ pyphetools/creation/hpo_exact_cr.py
@@ -64,10 +64,10 @@ class HpoExactConceptRecognizer(HpoConceptRecognizer):
         return [chunk.strip().lower() for chunk in chunks]
     
     def _parse_chunk(self, chunk, custom_d) -> List[HpTerm]:
-        if chunk in self._label_to_id:
-            hpo_id = self._label_to_id.get(chunk)
+        if chunk in custom_d:
+            hpo_id = custom_d.get(chunk)
             label = self._id_to_primary_label.get(hpo_id)
-            return [HpTerm(id=hpo_id, label=label)]
+            # return [HpTerm(id=hpo_id, label=label)]
         else:
             results = []
             # If we get here, we do two things
@@ -82,14 +82,14 @@ class HpoExactConceptRecognizer(HpoConceptRecognizer):
                     hpo_label = v
                     hpo_label_lc = hpo_label.lower()    
                     hpo_id = self._label_to_id.get(hpo_label_lc)
-                    if hpo_id is None:
-                        print(f"Unable to retrieve HPO Id for custom mapping {chunk} -> {hpo_label}")
-                        return []
+                    # if hpo_id is None:
+                    #     print(f"Unable to retrieve HPO Id for custom mapping {chunk} -> {hpo_label}")
+                    #     return []
                     results.append(HpTerm(id=hpo_id, label=hpo_label))
                     remaining_text = remaining_text.replace(key, " ")
             # When we get here, we look for HPO terms in the remaining text
             if len(remaining_text) > 5:
-                for k, v in self._label_to_id.items():
+                for k, v in custom_d.items():
                     key = k.lower()
                     if key in remaining_text:
                         hpo_id = v
@@ -117,6 +117,7 @@ class HpoExactConceptRecognizer(HpoConceptRecognizer):
             for chunk in chunks:
                 # Note that chunk has been stripped of whitespace and lower-cased already
                 res = self._parse_chunk(chunk=chunk, custom_d=custom_d)
+                print(res)
                 results.extend(res)
             return results
 
