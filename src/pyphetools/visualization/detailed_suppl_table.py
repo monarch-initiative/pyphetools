dee97f3131bc394875ba4af0aaf0181043aea1c3
pnrobinson <peter.robinson@jax.org>
2023-09-19 17:44:54 -0400

    comparative table output

--- src/pyphetools/visualization/detailed_suppl_table.py
+++ src/pyphetools/visualization/detailed_suppl_table.py
@@ -25,7 +25,7 @@ class DetailedSupplTable:
             raise ValueError(f"patient_d argument must be dictionary but was {type(patient_d)}")
         my_simple_patient_d = defaultdict(SimplePatient)
         for k, v in patient_d.items():
-            if str(type(v)) != "<class 'phenopackets.schema.v2.phenopackets_pb2.Phenopacket'>":
+            if str(type(v)) == "<class 'phenopackets.schema.v2.phenopackets_pb2.Phenopacket'>":
                 my_simple_patient_d[k] = SimplePatient(ga4gh_phenopacket=v)
             else:
                 raise ValueError(f"patient_d values must be GA4GH Phenopackets but was {type(v)}")
@@ -44,7 +44,7 @@ class DetailedSupplTable:
                 # key is a string such as HP:0001234, value is an HpTerm object
                 # we need to convert it to an object from hpo-toolkit because get_ancestors returns HpTerm objects
                 hp_termid = TermId.from_curie(hp_id)
-                ancs = self._hp_ontology.get_ancestors(hp_termid)
+                ancs = self._hp_ontology.graph.get_ancestors(hp_termid)
                 anc_set.add(hp_termid)
                 anc_set.update(ancs)
                 self._total_counts[hp_id] += 1
@@ -53,8 +53,8 @@ class DetailedSupplTable:
                     continue
                 else:
                     self._total_counts_propagated[hp_id.value] += 1
-            variant_d = pat.get_variant_d()
-            for var in variant_d.keys():
+            variants = pat.get_variant_list()
+            for var in variants:
                 var_d[var] += 1
 
             # TODO figure out what to do with biallelic
@@ -98,39 +98,63 @@ class DetailedSupplTable:
     @staticmethod
     def _get_counts(hpo_term_id:str, counts_d:dict):
         M = len(counts_d)
-        N = sum([1 for x in counts_d.values if x.contains_observed_term_id(hpo_term_id)])
+        N = 0
+        for k, v in counts_d.items():
+            if v.contains_observed_term_id(hpo_term_id):
+                N += 1
         return N, M
     
+    @staticmethod
+    def _get_counts_dict(pat_list):
+        counts_d = defaultdict(int)
+        for pat in pat_list:
+            observed_hpo = pat.get_observed_hpo_d()
+            for hpo_term_id in observed_hpo.keys():
+                counts_d[hpo_term_id] += 1
+        return counts_d   
+    
 
-    def get_html_table_by_pmid(self):
+    def get_html_table_by_pmid(self, min_count=0):
+        """
+        :param min_count: minimum count to be displayed ion the table (by default, all terms are displayed)
+        :type min_count: int
+        """
         by_pmid_d = defaultdict(list)
         all_observed_hpo = defaultdict(int) # HPO ids observed in our cohort
+        hpo_id_to_display_d = defaultdict()
         for pat in self._simple_patient_d.values():
-            for hpo_term_id in pat.get_observed_hpo_d.values():
+            observed_hpo_d = pat.get_observed_hpo_d()
+            for hpo_term in observed_hpo_d.values():
+                hpo_term_id = hpo_term.id
                 all_observed_hpo[hpo_term_id] += 1
+                hpo_id_to_display_d[hpo_term_id] = hpo_term
             if pat.has_pmid():
-                by_pmid_d[pat.get_pmid()] = pat 
+                by_pmid_d[pat.get_pmid()].append(pat) 
             else:
-                by_pmid_d["n/a"] = pat 
-        sorted_pmids = sorted(by_pmid_d.items(), key=lambda x:x[1], reverse=True) # sort by values
+                by_pmid_d["n/a"].append(pat) 
+        sorted_pmids = sorted(by_pmid_d.items(), key=lambda x:len(x[1]), reverse=True) # sort by values
+        sorted_pmids = [ x[0] for x  in sorted_pmids]
         sorted_hpos = sorted(all_observed_hpo.items(), key=lambda x:x[1], reverse=True)
+        sorted_hpos = [ x[0] for x  in sorted_hpos if x[1] > min_count]
         table_items = []
         table_items.append('<table style="border: 2px solid black;">\n')
-        table_items.append(f'<caption>{capt}</caption>\n')
-        table_items.append("<tr><th>HPO term</th>\n")
         middle = []
         for pmid in sorted_pmids:
             middle.append(f"<th>{pmid}</th>")
         middle_txt = "".join(middle)
         header = f"<tr><th>HPO term</th>{middle_txt}</tr>"
+        table_items.append(header)
         for hpo_term_id in sorted_hpos:
+            hpo_term = hpo_id_to_display_d.get(hpo_term_id)
             line_items = []
-            line_items.append(f"<tr><td>{hpo_term_id}</td>")
+            line_items.append(f"<tr><td>{hpo_term}</td>")
             for pmid in sorted_pmids:
-                counts_d = by_pmid_d.get(pmid)
-                N, M = DetailedSupplTable._get_counts(hpo_term_id=hpo_term_id, counts_d=counts_d)
+                simple_pat_list = by_pmid_d.get(pmid)
+                counts_d = DetailedSupplTable._get_counts_dict(simple_pat_list)
+                M = len(simple_pat_list)
+                N = counts_d.get(hpo_term_id, 0)
                 cell_contents = f"{N}/{M} ({100*N/M:.1f}%)"
-                line_items.append(cell_contents)
+                line_items.append(f"<td>{cell_contents}</td>")
             line_items.append("</tr>\n")
             table_items.append("".join(line_items))
         table_items.append('</table>\n') # close table content
--- src/pyphetools/visualization/phenopacket_ingestor.py
+++ src/pyphetools/visualization/phenopacket_ingestor.py
@@ -16,16 +16,14 @@ class PhenopacketIngestor:
             raise ValueError(f"indir argument {indir} must be directory!")
         self._indir = indir
         self._phenopackets = []
-        for root, dirs, files in os.walk(indir):
-            for file in files:
-                if file.endswith(".json"):
-                    if not os.path.isfile(file):
-                        raise FileNotFoundError(f"Could not find phenopacket file at '{file}'") 
-                    with open(file) as f:
-                        data = f.read()
-                        jsondata = json.loads(data)
-                        ppack = Parse(json.dumps(jsondata), PPKt.Phenopacket())
-                        self._phenopackets.append(ppack)
+        for f in os.listdir(indir):
+            fname = os.path.join(indir, f)
+            if fname.endswith(".json") and os.path.isfile(fname):
+                with open(fname) as f:
+                    data = f.read()
+                    jsondata = json.loads(data)
+                    ppack = Parse(json.dumps(jsondata), PPKt.Phenopacket())
+                    self._phenopackets.append(ppack)
         print(f"[pyphetools] Ingested {len(self._phenopackets)} GA4GH phenopackets.")
 
 
